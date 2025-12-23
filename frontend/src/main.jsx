449eccaf7c6080be28106eee5fbbc5ec28e5cfe7
Instagram-user-identity <mrpiyushchaudhari2006@gmail.com>
2025-12-23 21:52:52 +0530

    update pwa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,10 @@ import { BrowserRouter } from 'react-router-dom'
 import AuthContext from './context/AuthContext.jsx'
 import { Toaster } from 'react-hot-toast'
 import ChatContext from './context/ChatContext.jsx'
+import {registerSW} from 'virtual:pwa-register'
+
+registerSW()
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,75 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      manifest: {
+        name: "BruhChat",
+        short_name: "BruhChat",
+        description: "welcome to our BruhChat",
+        theme_color: "#57595B",
+        icons: [
+          {
+            src: "icons/Chat_icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "icons/Chat_icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "icons/Chat_icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "icons/Chat_icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "icons/Chat_icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "icons/Chat_icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "icons/Chat_icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/Chat_icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "icons/Chat_icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "icons/Chat_icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        start_url:".",
+        display:"standalone"
+      },
+      registerType:"prompt"
+    }),
+  ],
+});
